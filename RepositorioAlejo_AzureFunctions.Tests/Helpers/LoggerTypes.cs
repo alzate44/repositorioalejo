fbaeefacabe6e5e084239656d9a8d45e17073c1b
Alejandro Alzate <alejo.15975355@gmail.com>
2021-08-23 23:33:53 -0500

    Unit test preparation

---
+++ RepositorioAlejo_AzureFunctions.Tests/Helpers/LoggerTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositorioAlejo_AzureFunctions.Tests.Helpers
+{
+    public enum LoggerTypes
+    {
+        Null,
+        List
+    }
+}
---
+++ RepositorioAlejo_AzureFunctions.Tests/Helpers/NullScope.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositorioAlejo_AzureFunctions.Tests.Helpers
+{
+    public class NullScope : IDisposable
+    {
+        public static NullScope Instance { get; } = new NullScope();
+        public void Dispose() { }
+
+        private NullScope()
+        {
+
+        }
+    }
+}
